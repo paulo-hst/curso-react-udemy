bd0c974e276c8937d3bc538c76d728faf9b9acd8
Paulo Teixeira <hst.paulo@gmail.com>
2021-01-18 19:56:55 -0300

    primeiro componente com jsx

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,13 @@
+import React from 'react'
+
 export default function Primeiro(){
-    return 'Primeiro componente'
+
+    const mensagem = 'Seja bem-vindo(a)!'
+
+    return (
+        <div>
+            <h2>Meu primeiro componente em React!</h2>
+            <p>{ mensagem }</p>
+        </div>
+    )
 }
